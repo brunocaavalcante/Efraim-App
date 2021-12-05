f743be2401b01219d15c5c68b558e57ad4e58ea9
Bruno Cavalcanti da Silva <bcsilva@SF.PREFEITURA.SP.GOV.BR>
2021-12-04 21:53:55 -0300

    Index Membros

--- lib/pages/membros/index_membro_page.dart
+++ lib/pages/membros/index_membro_page.dart
@@ -1,4 +1,5 @@
 import 'package:app_flutter/theme/app-colors.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class IndexMembroPage extends StatefulWidget {
@@ -9,22 +10,45 @@ class IndexMembroPage extends StatefulWidget {
 }
 
 class _IndexMembroPageState extends State<IndexMembroPage> {
+  final Stream<QuerySnapshot> _membroStream =
+      FirebaseFirestore.instance.collection('membros').snapshots();
+
+  getMembros() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _membroStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Erro!');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Carregando");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data()! as Map<String, dynamic>;
+            return ListTile(
+              leading: const Icon(
+                Icons.person_sharp,
+                size: 30,
+              ),
+              title: Text(data['Nome']),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Membros"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Testando app no navegador',
-            ),
-          ],
-        ),
-      ),
+      body: getMembros(),
       floatingActionButton: FloatingActionButton(
         onPressed: null,
         backgroundColor: AppColors.blue,
--- lib/theme/app-colors.dart
+++ lib/theme/app-colors.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static Color get primary => Colors.yellow;
   static Color get accent => Colors.red;
-  static Color get blue => const Color.fromRGBO(88, 120, 174, 1);
+  static Color get blue => Color.fromRGBO(88, 120, 174, 1);
 }
